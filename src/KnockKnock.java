83e96cd501c578b27ece7d0cf692873190ae31c0
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-05-10 13:16:09 -0700

    KnockvKnock , Morning Zombie , Greeter

---
+++ src/KnockKnock.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+
+public class KnockKnock {
+public static void main(String[] args) {
+	
+JOptionPane.showMessageDialog(null, "Knock Knock");
+String answer = JOptionPane.showInputDialog("who's there ");
+String who = JOptionPane.showInputDialog(answer + "who?");
+JOptionPane.showMessageDialog(null, "moo");
+Sting moo
+
+}
+}
